8fd9170f8440e4297d8309c7845b0f4c6f739431
Gabriela Ortiz <73257443+Gabriela-Ortiz@users.noreply.github.com>
2023-07-06 19:37:12 -0600

    Shaba repository

---
+++ app/src/main/java/com/example/shaba/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.shaba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+        //var imageView= findViewById(R.id.foto)
+    }
+}
