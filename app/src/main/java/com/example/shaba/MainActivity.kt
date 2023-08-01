5a592f54184e5cf8c80be3f0a6a214583a504cf4
Gabriela Ortiz <73257443+Gabriela-Ortiz@users.noreply.github.com>
2023-07-31 19:31:39 -0600

    main activity fist method

--- app/src/main/java/com/example/shaba/MainActivity.kt
+++ app/src/main/java/com/example/shaba/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_screen)
+        setContentView(R.layout.loggin_screen)
         //var imageView= findViewById(R.id.foto)
     }
 }
