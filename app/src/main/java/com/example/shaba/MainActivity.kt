79dc4fbb7f92dc8588861fb646a9d2516afbe5ec
Gabriela Ortiz <73257443+Gabriela-Ortiz@users.noreply.github.com>
2023-08-28 21:51:05 -0600

    view modifications added in most of the activities

--- app/src/main/java/com/example/shaba/MainActivity.kt
+++ app/src/main/java/com/example/shaba/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.shaba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.ViewGroup
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.loggin_screen)
-        //var imageView= findViewById(R.id.foto)
+        setContentView(R.layout.activity_main)
     }
+
+
 }
